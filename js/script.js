c21febccc3aeee27b6b9a45543cb2092ba4f97d9
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-07-19 16:57:33 +0300

    qwer

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’.
+
+let result = 5+5 + '5'
+console.log(result);
+
+
+//Створіть змінну email з вашою електронною адресою. Напишіть скрипт, який перевіряє чи містить змінна email символ @ і рахує загальну кількість символів. Результат виведіть в консоль.
+
+let email = "bekerskijnazar9@gmail.com"
+let checkGmail = email.includes("@")
+let totalLenght = email.length;
+
+console.log(checkGmail);
+console.log(totalLenght);
+
+
+//Записати в окремих змінних кожне слово: My, name, is. Поєднати всі слова в змінну fullName. До змінної fullName додати ‘Viktor’. fullName вивести в консоль.
+
+let a = "My"
+let b = "name"
+let c = "is"
+
+let fullName = a +" "+ b +" "+ c;
+fullName += " Viktor"
+console.log(fullName);
+
+// Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати. За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього: «Дякуємо, Олександро! До сплати 300 гривень»
+
+let userName = "Nazar"
+let payment = 300;
+
+alert("Дякуємо, Nazar! До сплати 300 гривень");
